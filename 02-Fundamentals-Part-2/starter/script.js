c7dc9d3c3a5d2e66c897d37c580655394a78cc40
Matt <mprevell97@gmail.com>
2023-05-18 13:52:00 +0100

    first coding challenge complete with proper functionality

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -83,16 +83,69 @@
 // console.log(fruitProcessor(2, 3))
 
 
-const calcAge = function (birthYear) {
-  return 2037 - birthYear;
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years.`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired!`);
+//     return -1;
+//   }
+//   // return retirement;
+//   // // return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1930, 'Matt'));
+
+// return statement immediately exits the function.
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+let doplphinsAvg = calcAverage(44, 23, 71);
+let koalasAvg = calcAverage(65, 54, 49);
+console.log(doplphinsAvg, koalasAvg)
+
+const checkWinner = function (doplphinsAvg, koalasAvg) {
+  if (doplphinsAvg >= koalasAvg * 2) {
+    console.log(`The Dolphins won with average points of ${doplphinsAvg}`)
+  } else if (koalasAvg >= doplphinsAvg * 2) {
+    console.log(`The Koalas won with average points of ${koalasAvg}`)
+  } else {
+    console.log('No one won!')
+  };
 }
 
-
-const yearsUntilRetirement = fucntion(birthYear, firstName) {
-  const age = calcAge(birthYear);
-const retirement = 65 - age;
-return retirement;
-  // return `${firstName} retires in ${retirement} years`;
-}
-
-console.log(yearsUntilRetirement(1997, 'Matt'));
+checkWinner(doplphinsAvg, koalasAvg);
